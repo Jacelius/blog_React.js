0dc34f08c314ae12478fb4f346d71e6a87292248
Silas Paaske <sipn@itu.dk>
2021-11-27 16:25:23 +0100

    started tsx app

---
+++ blog/src/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+        <div className="nav">
+            <div className="name">
+                Silas Paaske
+            </div>
+            
+        </div>
+    </div>
+  );
+}
+
+export default Header;
