12b146661601d67b4491744e680e5e233ff7175b
Silas Paaske <sipn@itu.dk>
2021-11-27 16:26:16 +0100

    start commit

---
+++ blog/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+import Header from './Header';
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
